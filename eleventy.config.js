a48e48fb447735cd074b10e0c9a932b85b935894
Macklehatton <macklehatton@gmail.com>
2026-06-10 16:08:51 -0500

    HtmlBasePlugin

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,13 @@
+module.exports = async function (eleventyConfig) {
+    const { HtmlBasePlugin } = await import("@11ty/eleventy");
+
+    eleventyConfig.addPlugin(HtmlBasePlugin);
+};
+
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("video");
     eleventyConfig.addPassthroughCopy("styles.css");
+    pathPrefix: "/portfolio/"
 };
 
-module.exports.config = {
-    pathPrefix: "/portfolio/"
-}
